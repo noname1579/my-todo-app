750c73ad63de10eae288e650a9c4c8860c7a15cf
noname1579 <ilyalashenko115@gmail.com>
2025-05-29 20:05:38 +0300

    изменил config для публикации на gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-todo-app/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/my-todo-app' : '',
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "My ToDo"
+  title: "My Todo App"
 }
 
 export default function RootLayout({
